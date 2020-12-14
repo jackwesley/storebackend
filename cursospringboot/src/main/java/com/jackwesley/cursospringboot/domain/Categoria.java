29cb8d3e808b8fd4a21c5c53ac125a69f6706f61
Jack Wesley <jackwesley@hotmail.com>
2020-12-14 20:30:47 -0300

    Criação da classe Produto e associação muito para muitos

--- cursospringboot/src/main/java/com/jackwesley/cursospringboot/domain/Categoria.java
+++ cursospringboot/src/main/java/com/jackwesley/cursospringboot/domain/Categoria.java
@@ -1,25 +1,37 @@
 package com.jackwesley.cursospringboot.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 public class Categoria implements Serializable {
+    private static final long serialVersionUID =1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String nome;
+
+    @ManyToMany(mappedBy = "categorias")
+    private List<Produto> produtos = new ArrayList<>();
 
     public Categoria(){}
 
     public Categoria(Integer id, String nome) {
         this.id = id;
         this.nome = nome;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
     }
 
     public Integer getId() {
