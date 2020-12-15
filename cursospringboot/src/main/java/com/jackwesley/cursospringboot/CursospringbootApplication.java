c369fe0de28b7ee1c7b7ecf2629c7e3a45e68dc6
Jack Wesley <jackwesley@hotmail.com>
2020-12-15 19:47:14 -0300

    Criando entidades estado e cidade, mapeamento entre elas  e repositórios

--- cursospringboot/src/main/java/com/jackwesley/cursospringboot/CursospringbootApplication.java
+++ cursospringboot/src/main/java/com/jackwesley/cursospringboot/CursospringbootApplication.java
@@ -1,8 +1,12 @@
 package com.jackwesley.cursospringboot;
 
 import com.jackwesley.cursospringboot.domain.Categoria;
+import com.jackwesley.cursospringboot.domain.Cidade;
+import com.jackwesley.cursospringboot.domain.Estado;
 import com.jackwesley.cursospringboot.domain.Produto;
 import com.jackwesley.cursospringboot.repositories.CategoriaRepository;
+import com.jackwesley.cursospringboot.repositories.CidadeRepository;
+import com.jackwesley.cursospringboot.repositories.EstadoRepository;
 import com.jackwesley.cursospringboot.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,6 +23,12 @@ public class CursospringbootApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootApplication.class, args);
@@ -40,8 +50,21 @@ public class CursospringbootApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().add(cat1);
 
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+
 	}
 }
---
+++ cursospringboot/src/main/java/com/jackwesley/cursospringboot/domain/Estado.java
@@ -0,0 +1,65 @@
+package com.jackwesley.cursospringboot.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Estado implements Serializable {
+    private static final long serialVersionUID =1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    private String nome;
+
+    @OneToMany(mappedBy = "estado")
+    private List<Cidade> cidades = new ArrayList<>();
+
+    public Estado(){}
+
+    public Estado(Integer id, String nome) {
+        Id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Cidade> getCidades() {
+        return cidades;
+    }
+
+    public void setCidades(List<Cidade> cidades) {
+        this.cidades = cidades;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Estado)) return false;
+        Estado estado = (Estado) o;
+        return getId().equals(estado.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
