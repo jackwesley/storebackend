539ef312a4b53fec2d0bece6098977958fb94f34
Jack Wesley <jackwesley@hotmail.com>
2021-01-04 19:23:42 -0300

    Criando endpoint para listagem de todas categorias utilizando paginação

--- cursospringboot/src/main/java/com/jackwesley/cursospringboot/resources/CategoriaResource.java
+++ cursospringboot/src/main/java/com/jackwesley/cursospringboot/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import com.jackwesley.cursospringboot.domain.Categoria;
 import com.jackwesley.cursospringboot.dto.CategoriaDTO;
 import com.jackwesley.cursospringboot.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -36,6 +37,21 @@ public class CategoriaResource {
         List<CategoriaDTO> categoriasDto = categorias.stream()
                 .map(categoria -> new CategoriaDTO(categoria))
                 .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(categoriasDto);
+    }
+
+    @RequestMapping(value= "/page",method = RequestMethod.GET)
+    public ResponseEntity<Page<CategoriaDTO>> findPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage,
+            @RequestParam(value = "orderBy", defaultValue = "nome")String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC")String direction){
+
+        Page<Categoria> categorias = categoriaService.findPage(page, linesPerPage, orderBy,direction);
+
+        Page<CategoriaDTO> categoriasDto = categorias
+                .map(categoria -> new CategoriaDTO(categoria));
 
         return ResponseEntity.ok().body(categoriasDto);
     }
--- cursospringboot/src/main/java/com/jackwesley/cursospringboot/services/CategoriaService.java
+++ cursospringboot/src/main/java/com/jackwesley/cursospringboot/services/CategoriaService.java
@@ -6,6 +6,9 @@ import com.jackwesley.cursospringboot.services.exceptions.DataIntegrityException
 import com.jackwesley.cursospringboot.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -50,5 +53,19 @@ public class CategoriaService {
 
     public List<Categoria> findAll() {
         return categoriaRepository.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page,
+                                    Integer linesPerPage,
+                                    String orderBy,
+                                    String direction){
+
+        PageRequest pageRequest = PageRequest.of(page,
+                linesPerPage,
+                Sort.Direction.valueOf(direction),
+                orderBy);
+
+        return categoriaRepository.findAll(pageRequest);
+
     }
 }
